704d8731ac2e7b4b87abe78c8122c54bb92b8954
Sergey Chuvashev <chuvashev@mqd.me>
2016-02-13 23:57:45 +0400

    DI stuff

---
+++ app/src/main/java/me/chuvashev/carlauncher/di/qualifers/Worker.java
@@ -0,0 +1,15 @@
+package me.chuvashev.carlauncher.di.qualifers;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sergey Chuvashev
+ * Date: 13/02/16
+ * Time: 23:48
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Worker {}
